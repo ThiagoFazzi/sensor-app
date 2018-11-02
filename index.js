fafae4159fd92af10e241af829442a092b0790dd
ThiagoFazzi <thiago@xtwo.com.br>
2018-11-02 22:37:46 +0100

    add sensor interval

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const axios = require('axios');
 
+var oldTemperature = 0
+
 setInterval(() => {
-    console.log("passei")
-    axios.post('https://test-api-221311.appspot.com/sensor', {
+    const newTempperature = temperature(0,2)
+    if(newTempperature !== oldTemperature )
+    //axios.post('https://test-api-221311.appspot.com/sensor', {
+    axios.post('http://localhost:8080/sensor', {
         id: 'ewfsd8723jkk3mpm43',
         status: 'online',
-        temp: 10
+        temp: newTempperature
     })
     .then(function (response) {
         console.log(response);
     })
     .catch(function (error) {
         console.log(error);
-    });   
+    });
+    oldTemperature = newTempperature   
 }, 3000);
- 
+
+const temperature = (min, max) => Math.floor(Math.random()*(max-min)+min);
