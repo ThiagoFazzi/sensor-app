f3e24d46c6f7a006dd49e62a855511b7a72a35fa
ThiagoFazzi <thiago@xtwo.com.br>
2018-11-06 11:25:31 +0100

    sensor updated

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
 const axios = require('axios');
 
-var oldTemperature = 0
+const createSensor = (macAddres, type, min, max) => {
 
-setInterval(() => {
-    const newTempperature = temperature(0,10)
-    if(newTempperature !== oldTemperature )
-    //axios.post('https://test-api-221311.appspot.com/sensor', {
-    axios.post('http://localhost:8080/sensor', {
-        id: 'ewfsd8723jkk3mpm43',
-        status: 'online',
-        temp: newTempperature
-    })
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
-    oldTemperature = newTempperature   
-}, 3000);
+  var oldTemperature = 0
+
+  var sensor = {
+    id: null,
+    status: 'offline',
+    type: type,
+    created: null
+  }
+
+  setInterval(() => {
+      if(sensor.id === null){
+        axios.post('http://localhost:8080/sensor/new', {
+          macAddres: macAddres,
+          type: type
+        })
+        .then((response)=> {
+          console.log(response.data.sensor)
+          sensor = {
+            id: response.data.sensor.id,
+            status: response.data.sensor.status,
+            type: response.data.sensor.type,
+            created: response.data.sensor.created
+          }
+        })
+        .catch((error) => console.log(error))
+      } else {
+
+        const newTempperature = temperature(min,max)
+        if(newTempperature !== oldTemperature )
+        //axios.post('https://test-api-221311.appspot.com/sensor', {
+        axios.post('http://localhost:8080/sensor', {
+          id: sensor.id,
+          status: sensor.status,
+          value: newTempperature,
+          type: type
+        })
+        .then(response => console.log(macAddres, response.data))
+        .catch(error => console.log(error))
+        
+        oldTemperature = newTempperature
+      }
+  }, 1000);
+}
 
 const temperature = (min, max) => Math.floor(Math.random()*(max-min)+min);
+
+createSensor('00-00-00-00-00-FF', 'Temperature', 0, 10)
+createSensor('00-00-00-00-FF-FF', 'Umit',0,100)
